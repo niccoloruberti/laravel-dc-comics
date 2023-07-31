6f8f2efa149fa302aa1c17e73a9de750377c28a9
niccoloruberti <nikiruberti@gmail.com>
2023-07-31 22:04:43 +0200

    aggiunto footer - es base completato

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,99 @@
-footer
+<div class="background">
+    <footer>
+        <!-- sezione comics, merchandise, subscription, shop locator, power visa -->
+        <div class="acquisti">
+            <div>
+                <img src="{{Vite::asset('resources/img/buy-comics-digital-comics.png')}}">
+                <span>digital comics</span>
+            </div>
+            <div>
+                <img src="{{Vite::asset('resources/img/buy-comics-merchandise.png')}}">
+                <span>dc merchandise</span>
+            </div>
+            <div>
+                <img src="{{Vite::asset('resources/img/buy-comics-subscriptions.png')}}">
+                <span>subscription</span>
+            </div>
+            <div>
+                <img src="{{Vite::asset('resources/img/buy-comics-shop-locator.png')}}">
+                <span>comic shop locator</span>
+            </div>
+            <div>
+                <img src="{{Vite::asset('resources/img/buy-dc-power-visa.svg')}}">
+                <span>dc power visa</span>
+            </div>
+        </div>
+        <div class="container">
+        <!-- sezione link utili -->
+        <div class="link-utili">
+            <!-- dc comics -->
+            <div>
+                <ul>
+                    <h3>dc comics</h3>
+                    <li>Characters</li>
+                    <li>Comics</li>
+                    <li>Movies</li>
+                    <li>TV</li>
+                    <li>Games</li>
+                    <li>Videos</li>
+                    <li>News</li>
+                </ul>
+            </div>
+            <!-- shop -->
+            <div>
+                <ul>
+                    <h3>shop</h3>
+                    <li>Shop DC</li>
+                    <li>Shop DC Collectibles</li>
+                </ul>
+            </div>
+            <!-- dc -->
+            <div>
+                <ul>
+                    <h3>dc</h3>
+                    <li>Terms Of Use</li>
+                    <li>Privacy policy (New)</li>
+                    <li>Ad Chioces</li>
+                    <li>Advertising</li>
+                    <li>Jobs</li>
+                    <li>Subscriptions</li>
+                    <li>Talent Workshops</li>
+                    <li>CPSC Certificates</li>
+                    <li>Ratings</li>
+                    <li>Shop Help</li>
+                    <li>Contact Us</li>
+                </ul>
+            </div>
+            <!-- sites -->
+            <div>
+                <ul>
+                    <h3>sites</h3>
+                    <li>DC</li>
+                    <li>MAD Magazine</li>
+                    <li>DC Kids</li>
+                    <li>DC Universe</li>
+                    <li>DC Power Visa</li>
+                </ul>
+            </div>
+        </div>
+        <!-- dc logo -->
+        <img src="{{Vite::asset('resources/img/dc-logo-bg.png')}}">
+        </div>
+        <!-- sezione collegamenti social -->
+        <div class="social">
+            <!-- sign up -->
+            <div>
+                <button>sign-up now!</button>
+            </div>
+            <!-- social -->
+            <div>
+                <span>follow us</span>
+                <img src="{{Vite::asset('resources/img/footer-facebook.png')}}">
+                <img src="{{Vite::asset('resources/img/footer-twitter.png')}}">
+                <img src="{{Vite::asset('resources/img/footer-youtube.png')}}">
+                <img src="{{Vite::asset('resources/img/footer-pinterest.png')}}">
+                <img src="{{Vite::asset('resources/img/footer-periscope.png')}}">
+            </div>
+        </div>
+    </footer>
+</div>
