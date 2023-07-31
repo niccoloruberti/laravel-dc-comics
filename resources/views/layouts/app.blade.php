4b8c1b711a338453860b76bcc26de1d0c48126cf
niccoloruberti <nikiruberti@gmail.com>
2023-07-31 15:59:23 +0200

    creata struttura contenuto

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Laravel</title>
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body>
+    {{-- header --}}
+    @include ('partials.header')
+    {{-- main content --}}
+    <main>
+        @yield('content')
+    </main>
+    {{-- footer --}}
+    @include ('partials.footer')
+</body>
+
+</html>
